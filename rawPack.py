f936cacec947bd0267cfadbced2c71a6fd59b0d3
Bardo Espinosa <bardoespinosaa@gmail.com>
2020-08-11 22:31:33 -0500

    implement activity monitor

--- rawPack.py
+++ rawPack.py
@@ -1,6 +1,22 @@
 import struct
+import re
 
 class pack:
-    __slots__ = "origin_mac", "origin_ip", "destination_mac", "destination_ip"
+    def __init__(self, data):
+        self.origin_mac = self.mac_format(data[:12])
+        self.dest_mac = self.mac_format(data[12:24])
+        self.origin_ip = self.ip_format(data[52:60])
+        self.dest_ip = self.ip_format(data[60:68])
 
-    
+
+    def mac_format(self, data):
+        mac_arr = re.findall("..", data) 
+        return ":".join(mac_arr)
+
+
+    def ip_format(self, data):
+        ip_arr = re.findall("..", data)
+        ip2 = []
+        for item in ip_arr:
+            ip2.append( str(int(item, 16)))
+        return ".".join(ip2)
--- sniffer.py
+++ sniffer.py
@@ -2,13 +2,16 @@
 IMPORTANT: In linux this requires super user credentials
 """
 import socket
+import rawPack
+import os
 
 conn = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(3))
 
 while True:
     raw_data, addr = conn.recvfrom(65536)
+    os.system("clear")
     print("*********************************************************************")
-    print(addr)
-    print(raw_data[:6].hex())
+    data = rawPack.pack( raw_data.hex())
+    print(f"{data.origin_ip} is sending data to {data.dest_ip}")
 
 #conn.recvfrom(3)[1][-1].hex()
